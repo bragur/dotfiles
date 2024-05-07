e82afdb6b4247ee56f7e0472647abf3038860f5e
Bragi Bergþórsson <bragur@gmail.com>
2024-05-07 19:54:27 +0000

    feat(nvim-spelling): Add spelling options

--- nvim-distros/.config/nvim-distros/lazyvim/lua/config/options.lua
+++ nvim-distros/.config/nvim-distros/lazyvim/lua/config/options.lua
@@ -1,4 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Get blinking cursor on input
 vim.o.guicursor = "n-v-c-sm:block,ci-ve:ver25,r-cr-o:hor20,i:ver100-blinkwait175-blinkoff150-blinkon175-Cursor/lCursor"
+
+-- Enable spelling
+vim.o.spelllang = "en_us"
+vim.o.spell = true
