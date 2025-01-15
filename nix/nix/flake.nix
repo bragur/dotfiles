32fece331ab4abd619c9432423f1730c16e58202
Bragi <bragur@MacBookAir.localdomain>
2025-01-15 01:18:48 +0000

    Add nix

---
+++ nix/nix/flake.nix
@@ -0,0 +1,137 @@
+{
+  description = "Bragi's nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    # mac-app-util.url = "github:hraban/mac-app-util"; # Check this out
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
+  let
+    configuration = { pkgs, config, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+	  pkgs.zsh
+	  pkgs.oh-my-posh
+	  pkgs.stow
+	  pkgs.fzf
+	  pkgs.zoxide
+	  pkgs.eza
+	  pkgs.bat
+	  pkgs.autojump
+	  pkgs.neovim
+	  pkgs.tmux
+	  pkgs.tmuxifier
+	  pkgs.mkalias
+	  pkgs.obsidian
+	  pkgs.slack
+	  pkgs.vscode
+	  # pkgs._1password-gui
+	  # pkgs.ghostty
+        ];
+
+      homebrew = {
+        enable = true;
+	brews = [
+	  "antidote"
+	  "mas"
+	];
+	casks = [
+	  "cursor"
+	  "hammerspoon"
+	  "ghostty"
+	  "1password"
+	  "hyperkey"
+	];
+	masApps = {
+	};
+	onActivation.cleanup = "zap";
+	onActivation.autoUpdate = true;
+	onActivation.upgrade = true;
+      };
+      
+	#      fonts.packages =
+	#        [
+	#   pkgs.maple-mono-NF
+	# ];
+      
+      system.activationScripts.applications.text = let
+        env = pkgs.buildEnv {
+          name = "system-applications";
+          paths = config.environment.systemPackages;
+          pathsToLink = "/Applications";
+        } ;
+      in
+        pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "Setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+	  mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+	  while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+	'';
+      
+      system.defaults = {
+        dock.autohide = true;
+	dock.persistent-apps = [
+	  "/Applications/Arc.app"
+	  "/System/Applications/Messages.app"
+	  "/System/Applications/Mail.app"
+	  "/System/Applications/Photos.app"
+	  "${pkgs.obsidian}/Applications/Obsidian.app"
+	  "/Applications/Cursor.app"
+	  "/Applications/Ghostty.app"
+	];
+	finder.FXPreferredViewStyle = "clmv";
+	loginwindow.GuestEnabled = false;
+	NSGlobalDomain.KeyRepeat = 2;
+      };
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."air" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+	nix-homebrew.darwinModules.nix-homebrew
+	{
+	  nix-homebrew = {
+	    enable = true;
+	    # Apple Silicon Only
+	    enableRosetta = true;
+	    # User owning the Homebrew prefix
+	    user = "bragur";
+	  };
+	}
+      ];
+    };
+  };
+}
